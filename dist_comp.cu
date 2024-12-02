0134c0aeaeb20eea494980b7cf37daaaa564f806
chirag <zvovov@gmail.com>
2024-12-01 19:11:43 -0800

    apery constant - not parallel

--- dist_comp.cu
+++ dist_comp.cu
@@ -33,55 +33,112 @@ __global__ void cal_apery(float *sum, int nbin, int offset, int nthreads, int nb
     }
 }
 
-int main(int argc,char **argv) {
-	int myid,nproc,nbin,tid;
-	float step,offset,pi=0.0,pig;
-	dim3 dimGrid(NUM_BLOCK,1,1);  // Grid dimensions (only use 1D)
-	dim3 dimBlock(NUM_THREAD,1,1);  // Block dimensions (only use 1D)
-	float *sumHost,*sumDev;  // Pointers to host & device arrays
-	int dev_used;
-
-	MPI_Init(&argc,&argv);
-	MPI_Comm_rank(MPI_COMM_WORLD,&myid);  // My MPI rank
-	MPI_Comm_size(MPI_COMM_WORLD,&nproc);  // Number of MPI processes
-	// nbin = NBIN/nproc;  // Number of bins per MPI process
-	// step = 1.0/(float)(nbin*nproc);  // Step size with redefined number of bins
-	// offset = myid*step*nbin;  // Quadrature-point offset
-
-	omp_set_num_threads(NUM_DEVICE); // One OpenMP thread per GPU device
-	nbin = NBIN/(nproc*NUM_DEVICE); // # of bins per OpenMP thread
-	step = 1.0/(float)(nbin*nproc*NUM_DEVICE);
-
-	#pragma omp parallel private(offset, sumHost, sumDev, tid, dev_used) reduction(+:pi)
-	{	
-		int mpid = omp_get_thread_num();
-		offset = (NUM_DEVICE*myid+mpid)*step*nbin; // Quadrature-point offset
-		cudaSetDevice(mpid%2);
-
-		// cudaSetDevice(myid%2);
-		size_t size = NUM_BLOCK*NUM_THREAD*sizeof(float);  //Array memory size
-		sumHost = (float *)malloc(size);  //  Allocate array on host
-		cudaMalloc((void **) &sumDev,size);  // Allocate array on device
-		cudaMemset(sumDev,0,size);  // Reset array in device to 0
-		// Calculate on device (call CUDA kernel)
-		cal_pi <<<dimGrid,dimBlock>>> (sumDev,nbin,step,offset,NUM_THREAD,NUM_BLOCK);
-		// Retrieve result from device and store it in host array
-		cudaMemcpy(sumHost,sumDev,size,cudaMemcpyDeviceToHost);
-		// Reduction over CUDA threads
-		for(tid=0; tid<NUM_THREAD*NUM_BLOCK; tid++)
-			pi += sumHost[tid];
-		pi *= step;
-		// CUDA cleanup
-		free(sumHost);
-		cudaFree(sumDev);
-		cudaGetDevice(&dev_used);
-		printf("myid = %d; mpid = %d: device used = %d; partial pi = %f\n", myid, mpid, dev_used, pi);
-	}
+// int main(int argc,char **argv) {
+// 	int myid,nproc,nbin,tid;
+// 	float step,offset,pi=0.0,pig;
 	
-	// Reduction over MPI processes
-	MPI_Allreduce(&pi,&pig,1,MPI_FLOAT,MPI_SUM,MPI_COMM_WORLD);
-	if (myid==0) printf("Apery's con = %f\n",pig);
+// 	float apery=0.0, apery_final;
 
-	MPI_Finalize();
-	return 0;
+// 	dim3 dimGrid(NUM_BLOCK,1,1);  // Grid dimensions (only use 1D)
+// 	dim3 dimBlock(NUM_THREAD,1,1);  // Block dimensions (only use 1D)
+// 	float *sumHost,*sumDev;  // Pointers to host & device arrays
+// 	int dev_used;
+
+// 	MPI_Init(&argc,&argv);
+// 	MPI_Comm_rank(MPI_COMM_WORLD,&myid);  // My MPI rank
+// 	MPI_Comm_size(MPI_COMM_WORLD,&nproc);  // Number of MPI processes
+// 	// nbin = NBIN/nproc;  // Number of bins per MPI process
+// 	// step = 1.0/(float)(nbin*nproc);  // Step size with redefined number of bins
+// 	// offset = myid*step*nbin;  // Quadrature-point offset
+
+// 	omp_set_num_threads(NUM_DEVICE); // One OpenMP thread per GPU device
+// 	nbin = NBIN/(nproc*NUM_DEVICE); // # of bins per OpenMP thread
+// 	step = 1.0/(float)(nbin*nproc*NUM_DEVICE);
+
+// 	#pragma omp parallel private(offset, sumHost, sumDev, tid, dev_used) reduction(+:pi)
+// 	{	
+// 		int mpid = omp_get_thread_num();
+// 		offset = (NUM_DEVICE*myid+mpid)*step*nbin; // Quadrature-point offset
+// 		cudaSetDevice(mpid%2);
+
+// 		// cudaSetDevice(myid%2);
+// 		size_t size = NUM_BLOCK*NUM_THREAD*sizeof(float);  //Array memory size
+// 		sumHost = (float *)malloc(size);  //  Allocate array on host
+// 		cudaMalloc((void **) &sumDev,size);  // Allocate array on device
+// 		cudaMemset(sumDev,0,size);  // Reset array in device to 0
+// 		// Calculate on device (call CUDA kernel)
+// 		cal_pi <<<dimGrid,dimBlock>>> (sumDev,nbin,step,offset,NUM_THREAD,NUM_BLOCK);
+// 		// Retrieve result from device and store it in host array
+// 		cudaMemcpy(sumHost,sumDev,size,cudaMemcpyDeviceToHost);
+// 		// Reduction over CUDA threads
+// 		for(tid=0; tid<NUM_THREAD*NUM_BLOCK; tid++)
+// 			pi += sumHost[tid];
+// 		pi *= step;
+// 		// CUDA cleanup
+// 		free(sumHost);
+// 		cudaFree(sumDev);
+// 		cudaGetDevice(&dev_used);
+// 		printf("myid = %d; mpid = %d: device used = %d; partial pi = %f\n", myid, mpid, dev_used, pi);
+// 	}
+	
+// 	// Reduction over MPI processes
+// 	MPI_Allreduce(&pi,&pig,1,MPI_FLOAT,MPI_SUM,MPI_COMM_WORLD);
+// 	if (myid==0) printf("Apery's con = %f\n",pig);
+
+// 	MPI_Finalize();
+// 	return 0;
+// }
+
+int main(int argc, char **argv) {
+    int myid, nproc, nbin, tid;
+    int offset;
+    float apery=0.0, apery_final;
+    dim3 dimGrid(NUM_BLOCK,1,1);  // Grid dimensions (only use 1D)
+    dim3 dimBlock(NUM_THREAD,1,1);  // Block dimensions (only use 1D)
+    float *sumHost,*sumDev;  // Pointers to host & device arrays
+    int dev_used;
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&myid);  // My MPI rank
+    MPI_Comm_size(MPI_COMM_WORLD,&nproc);  // Number of MPI processes
+
+    omp_set_num_threads(NUM_DEVICE); // One OpenMP thread per GPU device
+    nbin = NBIN/(nproc*NUM_DEVICE); // # of terms per OpenMP thread
+	
+    #pragma omp parallel private(offset, sumHost, sumDev, tid, dev_used) reduction(+:apery)
+    {   
+        int mpid = omp_get_thread_num();
+        offset = (NUM_DEVICE*myid+mpid)*nbin; // Term offset for this thread
+
+        cudaSetDevice(mpid%2);
+
+        size_t size = NUM_BLOCK*NUM_THREAD*sizeof(float);  //Array memory size
+        sumHost = (float *)malloc(size);  //  Allocate array on host
+        cudaMalloc((void **) &sumDev,size);  // Allocate array on device
+        cudaMemset(sumDev,0,size);  // Reset array in device to 0
+
+        // Calculate on device (call CUDA kernel)
+        cal_apery <<<dimGrid,dimBlock>>> (sumDev,nbin,offset,NUM_THREAD,NUM_BLOCK);
+
+        // Retrieve result from device and store it in host array
+        cudaMemcpy(sumHost,sumDev,size,cudaMemcpyDeviceToHost);
+
+        // Reduction over CUDA threads
+        for(tid=0; tid<NUM_THREAD*NUM_BLOCK; tid++)
+            apery += sumHost[tid];
+
+        // CUDA cleanup
+        free(sumHost);
+        cudaFree(sumDev);
+        cudaGetDevice(&dev_used);
+        printf("myid = %d; mpid = %d: device used = %d; partial sum = %f\n", 
+               myid, mpid, dev_used, apery);
+    }
+    
+    // Reduction over MPI processes
+    MPI_Allreduce(&apery,&apery_final,1,MPI_FLOAT,MPI_SUM,MPI_COMM_WORLD);
+    if (myid==0) printf("Apery's constant = %f\n",apery_final);
+
+    MPI_Finalize();
+    return 0;
 }
